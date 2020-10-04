d5f88e285947b6ad0eae18a3e38827805352c52d
gaabrielreis <52452555+gaabrielreis@users.noreply.github.com>
2020-10-04 02:58:27 -0300

    Variaveis

---
+++ Java/ProjetoSwingInterfaceGrafica/src/Senha.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Senha {
+    
+    private boolean BCaixa1;
+    private boolean BCaixa2;
+    private boolean BCaixa3;
+    private boolean BCaixa4;
+    
+    private boolean BComum;
+    private boolean BRapido;
+    private boolean BPrioritario;
+    
+    private String TSenhaGerada;
+    private String TCaixaParaAtendimento;
+    private String TSenhaParaAtendimento;
+    
+}
